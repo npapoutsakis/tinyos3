3e20b0f8831b39b47e9c6fb33c22280d8dc6fea3
GeorgeVal <georgevalavanis01@gmail.com>
2021-11-13 21:22:27 +0200

    Added rlnodes to PTCB and PCB and changed initialize_PCB

--- kernel_sched.h
+++ kernel_sched.h
@@ -121,7 +121,7 @@ typedef struct thread_control_block {
 
 	PCB* owner_pcb; /**< @brief This is null for a free TCB */
 
-  	PTCB* ptcb; 
+  PTCB* ptcb; 
 
 	cpu_context_t context; /**< @brief The thread context */
 	Thread_type type; /**< @brief The type of thread */
